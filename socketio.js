24c9bd7a40074dbd7222ea956d43f62d915ffe73
ArcherGu <649601376@qq.com>
2021-03-14 14:04:11 +0800

    add: Socket.IO Emit/On/Init

--- socketio.js
+++ socketio.js
@@ -1,10 +1,14 @@
 module.exports = function (RED) {
-    var socketio = require("socket.io");
+    const socketio = require("socket.io");
+
+    function _getNamespaceInstance(instance, namespace) {
+        return namespace ? instance.of(namespace) : instance;
+    }
 
     function socketIoInstance(n) {
         RED.nodes.createNode(this, n);
         // node-specific code goes here
-        var node = this;
+        const node = this;
         this.name = n.name;
         this.port = n.port;
         try {
@@ -17,36 +21,72 @@ module.exports = function (RED) {
         this.instance = socketio(this.port, this.options);
         node.log("Created Socket.IO server at " + this.port);
 
-        node.on("close", function () {
+        node.on("close", () => {
             this.instance.close();
             node.log("Closed Socket.IO server at " + this.port);
         });
     }
 
+    function socketIoInit(n) {
+        RED.nodes.createNode(this, n);
+        // node-specific code goes here
+        const node = this;
+        this.name = n.name;
+        this.delay = n.delay;
+        this.instance = RED.nodes.getNode(n.instance).instance;
+
+        setTimeout(() => {
+            node.send({
+                socketIoInstance: this.instance
+            });
+        }, this.delay);
+    }
+
     function socketIoOn(n) {
         RED.nodes.createNode(this, n);
         // node-specific code goes here
-        var node = this;
+        const node = this;
         this.name = n.name;
-        this.instance = RED.nodes.getNode(n.instance).instance;
         this.event = n.event;
         this.namespace = n.namespace;
 
-        var currentIo;
-        if (this.namespace) {
-            this.instance = this.instance.of(this.namespace);
-        }
-        else {
-            currentIo = this.instance;
-        }
+        node.on("input", (msg) => {
+            if (!msg.socketIoInstance) {
+                node.log("No msg.instance");
+                return;
+            }
+            const instance = _getNamespaceInstance(msg.socketIoInstance, this.namespace);
 
-        currentIo.on(this.event, (socket) => {
-            node.send({
-                socket
+            const listener = (socket) => {
+                msg.socket = socket;
+                node.send(msg);
+            };
+
+            instance.on(this.event, listener);
+
+            node.on("close", () => {
+                instance.off(this.event, listener);
             });
         });
     }
 
+    function socketIoEmit(n) {
+        RED.nodes.createNode(this, n);
+        // node-specific code goes here
+        const node = this;
+        this.name = n.name;
+        this.event = n.event;
+        this.namespace = n.namespace;
+        this.instance = RED.nodes.getNode(n.instance).instance;
+
+        const instance = _getNamespaceInstance(this.instance, this.namespace);
+        node.on('input', (msg) => {
+            instance.emit(this.event, msg.payload);
+        });
+    }
+
     RED.nodes.registerType("socket.io-instance", socketIoInstance);
+    RED.nodes.registerType("socket.io-init", socketIoInit);
     RED.nodes.registerType("socket.io-on", socketIoOn);
+    RED.nodes.registerType("socket.io-emit", socketIoEmit);
 };
